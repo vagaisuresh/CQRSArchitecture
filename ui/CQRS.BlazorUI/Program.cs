1282b73f5b8a306f417c3df60dc3137a9f034ebf
vagaisuresh <vagaisuresh@yahoo.co.in>
2025-08-08 12:13:50 +0530

    Store and retrieve the ToDo list in the local storage

--- ui/CQRS.BlazorUI/Program.cs
+++ ui/CQRS.BlazorUI/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 using CQRS.BlazorUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
